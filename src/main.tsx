8e2bb82159539a02bdbc8cb7cb9ae51a48cc0c0c
Andy Lee <a.lee.intl@gmail.com>
2023-09-19 22:25:02 +0200

    Unset React.StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
-import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "modern-normalize/modern-normalize.css"
 import "./styles/global.css"
 import "./styles/preflight.css"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />)
