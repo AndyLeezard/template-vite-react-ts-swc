2c395067d7d37ad09f3b8f9b3e7d91bdac88df83
Andy Lee <a.lee.intl@gmail.com>
2023-09-19 21:45:45 +0200

    Provide base css setup with modern-normalize

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
-import "./index.css"
+import "modern-normalize/modern-normalize.css"
+import "./styles/global.css"
+import "./styles/preflight.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
